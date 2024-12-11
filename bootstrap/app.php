3c996bc2b813b650ee6f7ecbd409d5cb7491b220
Hammed Oyedele <itz.harmid@gmail.com>
2024-12-11 08:15:18 +0100

    fix: csrf token validation for endpoints

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,15 +5,21 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(dirname(__DIR__))
-                  ->withRouting(
-                      web: __DIR__.'/../routes/web.php',
-                      commands: __DIR__.'/../routes/console.php',
-                      health: '/',
-                  )
-                  ->withMiddleware(function (Middleware $middleware) {
-                      //
-                  })
-                  ->withExceptions(function (Exceptions $exceptions) {
-                      //
-                  })
-                  ->create();
+    ->withRouting(
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
+        health: '/',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->validateCsrfTokens(except: [
+            '/user',
+            '/login',
+            '/register',
+            '/debit',
+            '/credit',
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })
+    ->create();
